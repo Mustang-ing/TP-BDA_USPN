24ec7ec67650db6390cdaa120327ae6dc02aef90
Mustang-ing <aissapansan@hotmail.com>
2025-04-26 11:28:39 +0200

    Initialisation of TP3 and TP5

---
+++ TP3/TP3_Project/Spring-TP3/src/main/java/com/example/springtp3/SpringTp3Application.java
@@ -0,0 +1,13 @@
+package com.example.springtp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTp3Application.class, args);
+	}
+
+}
---
+++ TP3/TP3_Project/Spring-TP3/src/test/java/com/example/springtp3/SpringTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
