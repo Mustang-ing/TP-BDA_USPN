f20c5892d971f31782923c2a7f2f8160a321f21e
Mustang-ing <aissapansan@hotmail.com>
2025-04-26 17:05:28 +0200

    Step 3 - API REST : Complete (Collection & Arraylist)

--- TP3/TP3_Project/Spring-TP3/src/main/java/com/example/springtp3/MyApp.java
+++ TP3/TP3_Project/Spring-TP3/src/main/java/com/example/springtp3/MyApp.java
@@ -1,12 +1,30 @@
 package com.example.springtp3;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //On définit une API Rest via cette annotation
 @RestController
 public class MyApp {
+
+	public static Collection<Etudiant> liste = new ArrayList();
+
+	static
+	{
+		liste.add(new  Etudiant(0,"A",19));
+		liste.add(new  Etudiant(1,"A",19));
+		liste.add(new  Etudiant(2,"A",19));
+		liste.add(new  Etudiant(3,"A",19));
+	}
 	
+	@GetMapping(value = "/liste")
+	public Collection<Etudiant> getAllEtudiant()
+	{
+		return liste;
+	}
 	//Implémentation des méthodes HTTP. La value précise le chemin de l'URL.
 	@GetMapping(value = "/b")
 	public String Bonjour()
@@ -26,4 +44,11 @@ public class MyApp {
 		return new Etudiant(1,"A",19);
 	}
 
+	//Attention, la méthode attend deux paramètre. Il faut donc les fournir via l'URL. ( /somme?a=1&b=1 )
+	//On peut utiliser PostMan aussi
+	@GetMapping(value = "/somme")
+	public double somme(double a, double b)
+	{
+		return a+b;
+	}
 }
